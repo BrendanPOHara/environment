34e90e3405f464001ea3c09759be1be46c01e920
Brendan O'Hara <lilmrbutterpantz@gmail.com>
2016-11-16 03:11:05 +0000

    Upgrade to newer cmake and add bash aliases

--- linux/cmake/configure_cmake.sh
+++ linux/cmake/configure_cmake.sh
@@ -1,1 +1,13 @@
-sudo apt-get install cmake
+curl -sSL https://cmake.org/files/v3.7/cmake-3.7.0.tar.gz | sudo tar -xzC /opt
+cd /opt/cmake-3.7.0/
+
+sudo ./bootstrap
+sudo make
+sudo make install
+
+cd -
+
+# If you are okay using an old version of cmake, just use apt-get.
+# sudo apt-get install cmake
+
+
