0ced3b51903d5a42af765fb67ab7dc9c7c435d48
Brendan O'Hara <lilmrbutterpantz@gmail.com>
2017-01-06 03:45:41 +0000

    Add sqlite and python

--- linux/configure.sh
+++ linux/configure.sh
@@ -8,8 +8,11 @@ function config_dir()
     echo "...Done Configuring \"$1\" Directory!";
 }
 
+config_dir bash
 config_dir git
 config_dir vim
 config_dir svn
 config_dir llvm
 config_dir cmake
+config_dir python
+config_dir sqlite3
